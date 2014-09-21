338b06d516344342444c4adeeb9804e8650c7d9a
Douglas <douglas.develop@gmail.com>
2014-09-21 12:16:15 -0300

    Adicionado configuração do Twig

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,23 @@
 <?php
 
-/**
- * Database configuration - PDO_instance
- */
-$database['type'] = 'mysql';
-$database['hostname'] = 'localhost';
-$database['username'] = 'root';
-$database['password'] = '102030';
-$database['database_name'] = 'test';
+class Config {
+
+	public function getDatabase() {
+
+		$database['type'] = 'mysql';
+		$database['hostname'] = 'localhost';
+		$database['username'] = 'root';
+		$database['password'] = '102030';
+		$database['database_name'] = 'test';
+
+		return $database;
+	}
+
+	public function getTemplate() {
+		Twig_Autoloader::register();
+		$loader = new Twig_Loader_Filesystem(APP.'/view/'); 
+		$twig = new Twig_Environment($loader); 
+		return $twig;
+	}
+
+}
