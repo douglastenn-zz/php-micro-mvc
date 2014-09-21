f3f7046c4edfc89282230a16a46c426580f737c0
Douglas <douglas.develop@gmail.com>
2014-09-21 12:19:50 -0300

    Arquivos para manipulação de rotas

---
+++ app/config/route.php
@@ -0,0 +1,22 @@
+<?php
+	class Router{
+		
+		public static function route(Request $request){
+			
+			$controller = $request->getController().'Controller';
+			$method = $request->getMethod();
+			$args = $request->getArgs();
+
+			$controllerFile = APP.'controllers/'.$controller.'.php';
+
+			$controller = new $controller;
+			$method = (is_callable(array($controller,$method))) ? $method : 'index';	
+			
+			if(!empty($args)){
+				call_user_func_array(array($controller,$method),$args);
+			}else{	
+				call_user_func(array($controller,$method));
+			}	
+			return;
+		}
+	}
